647252b1d2f7cd3b67da26adabaacbe0a530996b
SuryayanMukhopadhyay <169067813+SuryayanMukhopadhyay@users.noreply.github.com>
2024-06-13 18:23:48 +0530

    Time: 0 ms (100%), Space: 40.2 MB (39.99%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+       int one = 1;
+       int two = 0;
+       for(int i=n-1;i>=0;i--)
+       {
+           int temp = one;
+           one = two + one;
+           two = temp;
+       } 
+       return one;
+    }
+}
