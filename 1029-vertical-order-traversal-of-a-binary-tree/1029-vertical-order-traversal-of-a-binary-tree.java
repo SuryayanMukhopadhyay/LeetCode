7f227e8336e49df928bfc1cca2d87c48551ee05a
SuryayanMukhopadhyay <169067813+SuryayanMukhopadhyay@users.noreply.github.com>
2024-06-29 13:43:43 +0530

    Time: 2 ms (99.45%), Space: 42.2 MB (83.18%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
@@ -1,92 +1,65 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-
- // Create class tuple to store the node and coordinates.
- class Tuple{
-    TreeNode node;
-    int row;
-    int col;
-    // Constructor for tuple.
-    public Tuple(TreeNode _node, int _row, int _col){
-        node = _node;
-        row = _row;
-        col = _col;
-    }
- }
 class Solution {
+    static int leastCol;
+    static int mostCol;
 
-    // We perform Level order trversal to get the output....
-
+    static List<List<Integer>> ans;
+    static PriorityQueue<Pair> pq;
+    
     public List<List<Integer>> verticalTraversal(TreeNode root) {
+        ans=new ArrayList<List<Integer>>();
 
-        // We need a treemap to store the vertical values(columns) and PriorityQueue to store the node values in increasing order.
-        // (x,y,node)
-        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map = new TreeMap<>();
+        bfs(root);
+        int sumCol=mostCol-leastCol+1;
 
-        // Create a queue for instering each node with respective row(x), column(y) values during iteration.
-        // Initially coordinates of node are...(node,x->(0),y->(0))
-        Queue<Tuple> q = new LinkedList<Tuple>();
-
-        // Insert the tuple
-        q.add(new Tuple(root,0,0));
-
-        // Loop untill queue is empty.
-        while(!q.isEmpty()){
-
-            // Pop the tuple from stack.
-            Tuple tuple = q.poll();
-
-            // Initialize the values inside the tuple.
-            TreeNode node = tuple.node;
-            int x = tuple.row;
-            int y = tuple.col;
-
-            // Insert the values into the treemap.
-
-            // x - > vertical coordinate --> check example test cases.
-            if(!map.containsKey(x)) map.put(x,new TreeMap<>());
-
-            // y - > horizontal coordinate --> check example test cases.
-            if(!map.get(x).containsKey(y)) map.get(x).put(y,new PriorityQueue<>());
-
-            // Finally insert node value (!!!not node!!!) into map inside PriorityQueue.
-            map.get(x).get(y).add(node.val);
-
-            // Check is there exists a left or right node to the node present in the queue.
-            // If present, then add it to the queue.
-            if(node.left!=null) q.add(new Tuple(node.left,x-1,y+1));
-            if(node.right!=null) q.add(new Tuple(node.right, x+1,y+1));
+        for(int i=0;i<sumCol;i++)
+            ans.add(new ArrayList<Integer>());
+        
+        while(!pq.isEmpty()){
+            Pair curr=pq.poll();
+            ((ArrayList<Integer>)ans.get(-leastCol+curr.col)).add(curr.node.val);
         }
-
-        // Create a List Of List to store the list of node values.
-        List<List<Integer>> list = new ArrayList<>();
-
-        // Loop through the map and add the values.
-        // x - > key, (y, nodes) -> values.
-        for(TreeMap<Integer,PriorityQueue<Integer>> yn : map.values()){
-            // Create a sublist to store node values in each vertical.
-            list.add(new ArrayList<>());
-
-            // Now iterate in the PriorityQueue.
-            for(PriorityQueue<Integer> nodes : yn.values()){
-                // Add node into the sublist from 
-                while(!nodes.isEmpty()){
-                    list.get(list.size()-1).add(nodes.poll());
-                }
+        
+        return (List<List<Integer>> )ans;
+    }
+    public static void bfs(TreeNode root){
+        Queue<Pair> q=new LinkedList<Pair>();
+        pq =new PriorityQueue<Pair>();
+        int row=0;
+        int col=0;
+        q.add(new Pair(row,col, root));
+        leastCol=0;
+        mostCol=0;
+        row=1;
+        
+        while(!q.isEmpty()){
+            Pair curr = q.poll();
+            pq.add(curr);
+            
+            if(curr.node.left!=null){
+                q.add(new Pair(curr.row+1, curr.col-1, curr.node.left));
+                leastCol=Math.min(leastCol, curr.col-1);
             }
-        }return list;
+            if(curr.node.right!=null){
+                q.add(new Pair(curr.row+1, curr.col+1, curr.node.right));
+                mostCol=Math.max(mostCol, curr.col+1);
+
+            }
+        }
+    }
+    
+    static class Pair implements Comparable<Pair>{
+        int col;
+        int row;
+        TreeNode node;
+        public Pair(int r,int c, TreeNode n){
+            row=r;
+            col=c;
+            node=n;
+        }
+        public int compareTo(Pair p){
+            if(col!=p.col) return col-p.col;
+            if(row!=p.row) return row-p.row;
+            return node.val-p.node.val;
+        }
     }
 }
