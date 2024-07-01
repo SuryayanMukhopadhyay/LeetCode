eb4a7c09c32be0ff8ea145067e63109eb69cd3e6
SuryayanMukhopadhyay <169067813+SuryayanMukhopadhyay@users.noreply.github.com>
2024-07-01 23:25:37 +0530

    Time: 633 ms (5.02%), Space: 45.4 MB (13.1%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    List<List<Integer>> list = new ArrayList<>();
+
+    public static void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
+    public void helper(int[] nums, int idx, int n) {
+        if(idx == n) {
+            if(!list.contains(IntStream.of(nums).boxed().collect(Collectors.toList())))
+                list.add(IntStream.of(nums).boxed().collect(Collectors.toList()));
+            return;
+        }
+        for(int i = idx; i < n; i++) {
+            swap(nums, idx, i);
+            helper(nums, idx + 1, n);
+            swap(nums, idx, i);
+        }
+        return;
+    }
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        int n = nums.length;
+        helper(nums, 0, n);
+        return list;
+    }
+}
