2696cbcf6f69ac16371be3e2b685b6fbfbed0a97
SuryayanMukhopadhyay <169067813+SuryayanMukhopadhyay@users.noreply.github.com>
2024-07-03 17:56:44 +0530

    Time: 4 ms (85.9%), Space: 68.5 MB (38.04%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,39 +1,40 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    ListNode reverse(ListNode head){
-        ListNode curr=head, prev=null;
-        while(curr!=null){
-            ListNode nextnode=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=nextnode;
+    ListNode reverse(ListNode head) {
+        ListNode curr = head, prev = null;
+        while (curr != null) {
+            ListNode nextnode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextnode;
         }
         return prev;
     }
+
     public boolean isPalindrome(ListNode head) {
-        ListNode slow=head, fast=head;
-        while(fast!=null&&fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        slow=reverse(slow);
-        fast=head;
-        while(slow!=null){
-            if(fast.val!=slow.val){
+        slow = reverse(slow);
+        fast = head;
+        while (slow != null) {
+            if (fast.val != slow.val) {
                 return false;
             }
-            slow=slow.next;
-            fast=fast.next;
+            slow = slow.next;
+            fast = fast.next;
         }
-     return true;   
+        return true;
     }
 }
