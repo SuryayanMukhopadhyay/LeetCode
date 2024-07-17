103c0675e7d6ecdcdbfba7e98244c7c68a999b85
SuryayanMukhopadhyay <169067813+SuryayanMukhopadhyay@users.noreply.github.com>
2024-07-17 10:40:01 +0530

    Time: 2 ms (99.9%), Space: 44.4 MB (64.56%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,40 +1,25 @@
 class Solution {
-    public boolean canFinish(int n, int[][] prerequisites) {
-        List<Integer>[] adj = new List[n];
-        int[] indegree = new int[n];
-        List<Integer> ans = new ArrayList<>();
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        final int[] prereqCount = new int[numCourses];
+        for (int[] req : prerequisites) prereqCount[req[1]]++;
 
-        for (int[] pair : prerequisites) {
-            int course = pair[0];
-            int prerequisite = pair[1];
-            if (adj[prerequisite] == null) {
-                adj[prerequisite] = new ArrayList<>();
-            }
-            adj[prerequisite].add(course);
-            indegree[course]++;
-        }
-
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
-                queue.offer(i);
-            }
-        }
-
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-            ans.add(current);
-
-            if (adj[current] != null) {
-                for (int next : adj[current]) {
-                    indegree[next]--;
-                    if (indegree[next] == 0) {
-                        queue.offer(next);
-                    }
+        final int length = prerequisites.length;
+        final boolean[] finished = new boolean[length];
+        boolean run = true;
+        while (run) {
+            run = false;
+            for (int i = 0; i < length; i++) {
+                if (!finished[i] && prereqCount[prerequisites[i][0]] == 0) {
+                    finished[i] = true;
+                    prereqCount[prerequisites[i][1]]--;
+                    run = true;
                 }
             }
         }
 
-        return ans.size() == n;
+        for (int i = 0; i < numCourses; i++) {
+            if (prereqCount[i] != 0) return false;
+        }
+        return true;
     }
 }
