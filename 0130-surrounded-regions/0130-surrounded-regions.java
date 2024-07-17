9b6767acd64f2a4055b0d12b1b4aa726cde83a25
SuryayanMukhopadhyay <169067813+SuryayanMukhopadhyay@users.noreply.github.com>
2024-07-17 09:17:52 +0530

    Time: 2 ms (86%), Space: 45.2 MB (67.17%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,31 +1,29 @@
 class Solution {
-    public static void helper(char[][] board, int i, int j, int m, int n) {
-        if(i < 0 || i == m || j < 0 || j == n || board[i][j] != 'O') {
+    public static void capture(char[][] board, int m, int n, int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != 'O')
             return;
-        }
         board[i][j] = 'T';
-        helper(board, i-1, j, m, n);
-        helper(board, i+1, j, m, n);
-        helper(board, i, j-1, m, n);
-        helper(board, i, j+1, m, n);
+        capture(board, m, n, i + 1, j);
+        capture(board, m, n, i - 1, j);
+        capture(board, m, n, i, j + 1);
+        capture(board, m, n, i, j - 1);
     }
+
     public void solve(char[][] board) {
         int m = board.length, n = board[0].length;
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(board[i][j] == 'O' && (i == 0 || i == m - 1) || j == 0 || j == n - 1) {
-                    helper(board, i, j, m, n);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O' && (i == 0 || i == m - 1 || j == 0 || j == n - 1)) {
+                    capture(board, m, n, i, j);
                 }
             }
         }
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(board[i][j] == 'O') {
+                if(board[i][j] == 'O')
                     board[i][j] = 'X';
-                }
-                else if(board[i][j] == 'T') {
+                else if(board[i][j] == 'T')
                     board[i][j] = 'O';
-                }
             }
         }
     }
