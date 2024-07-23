0cb3a0cf21294f5a932bef6d2b11d3bb27c9c4c3
SuryayanMukhopadhyay <169067813+SuryayanMukhopadhyay@users.noreply.github.com>
2024-07-23 09:57:05 +0530

    Time: 87 ms (87.09%), Space: 49.1 MB (43.95%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -4,14 +4,14 @@ class Solution {
 
         int size = (int) Math.sqrt(n);
         for (int j = 2; j <= size; j++) {
-            if (prime[j] == false) {
+            if (!prime[j]) {
                 for (int i = j * j; i < n; i += j)
                     prime[i] = true;
             }
         }
         int count = 0;
         for (int i = 2; i < n; i++) {
-            if (prime[i] == false)
+            if (!prime[i])
                 count++;
         }
         return count;
