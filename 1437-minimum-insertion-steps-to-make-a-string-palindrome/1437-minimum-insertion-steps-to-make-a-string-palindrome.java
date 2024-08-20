063e34f5f8dacb39ea39cf5e8cc48f94bc9854a1
SuryayanMukhopadhyay <169067813+SuryayanMukhopadhyay@users.noreply.github.com>
2024-08-20 07:52:51 +0530

    Time: 34 ms (18.44%), Space: 45.7 MB (11.43%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -9,30 +9,27 @@ class Solution {
             right--;
         }
     }
-    public int lcs(String s, StringBuilder rev, int m, int n, int[][] memo) {
-        if(m == 0 || n == 0) {
-            return 0;
-        }
-        if(memo[m][n] != -1) {
-            return memo[m][n];
-        }
-        else {
-            if(s.charAt(m - 1) == rev.charAt(n - 1)) {
-                return memo[m][n] = 1 + lcs(s, rev, m - 1, n - 1, memo);
-            }
-            else return memo[m][n] = Math.max(lcs(s, rev, m - 1, n, memo), lcs(s, rev, m, n - 1, memo));
-        }
-    }
     public int minInsertions(String s) {
         StringBuilder rev = new StringBuilder(s);
         reverse(rev);
         int n = s.length();
         int[][] memo = new int[n + 1][n + 1];
         for(int i = 0; i < n + 1; i++) {
-            for(int j = 0; j < n + 1; j++) {
-                memo[i][j] = -1;
+            memo[i][0] = 0;
+        }
+        for(int i = 0; i < n + 1; i++) {
+            memo[0][i] = 0;
+        }
+        for(int i = 1; i < n + 1; i++) {
+            for(int j = 1; j < n + 1; j++) {
+                if(s.charAt(i - 1) == rev.charAt(j - 1)) {
+                    memo[i][j] = 1 + memo[i - 1][j - 1];
+                }
+                else {
+                    memo[i][j] = Math.max(memo[i - 1][j], memo[i][j - 1]);
+                }
             }
         }
-        return n - lcs(s, rev, n, n, memo);
+        return n - memo[n][n];
     }
 }
