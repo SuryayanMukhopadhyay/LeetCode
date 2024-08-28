617d00a14d11ac88a89eb08bf108341d400fdd92
SuryayanMukhopadhyay <169067813+SuryayanMukhopadhyay@users.noreply.github.com>
2024-08-28 09:51:56 +0530

    Time: 5 ms (41.26%), Space: 43.9 MB (96.97%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,45 +9,50 @@
  * }
  */
 class Solution {
-    public ListNode merge(ListNode l1, ListNode l2) {
-        ListNode prehead = new ListNode(-1);
-        ListNode cur = prehead;
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                cur.next = l1;
-                l1 = l1.next;
-            } else {
-                cur.next = l2;
-                l2 = l2.next;
-            }
-            cur = cur.next;
-        }
-        cur.next = l1 == null ? l2 : l1;
-        return prehead.next;
-    }
+    // public ListNode merge(ListNode l1, ListNode l2) {
+    // ListNode prehead = new ListNode(-1);
+    // ListNode cur = prehead;
+    // while (l1 != null && l2 != null) {
+    // if (l1.val <= l2.val) {
+    // cur.next = l1;
+    // l1 = l1.next;
+    // } else {
+    // cur.next = l2;
+    // l2 = l2.next;
+    // }
+    // cur = cur.next;
+    // }
+    // cur.next = l1 == null ? l2 : l1;
+    // return prehead.next;
+    // }
 
     public ListNode mergeKLists(ListNode[] lists) {
         PriorityQueue<ListNode> PQ = new PriorityQueue<>((x, y) -> x.val - y.val);
         ListNode list = null;
-        if(lists.length == 0) {
+        if (lists.length == 0) {
             return list;
         }
-        if(lists.length == 1) {
+        if (lists.length == 1) {
             return lists[0];
         }
-        for(ListNode l : lists) {
-            if(l != null) {
+        for (ListNode l : lists) {
+            while(l != null) {
                 PQ.offer(l);
+                l = l.next;
             }
         }
-        while(!PQ.isEmpty()) {
-            ListNode l1 = PQ.poll();
-            if(!PQ.isEmpty()) {
-                ListNode l2 = PQ.poll();
-                ListNode temp = merge(l1, l2);
-                PQ.offer(temp);
+        ListNode prev = null;
+        while (!PQ.isEmpty()) {
+            ListNode l = PQ.poll();
+            l.next = null;
+            if(list == null) {
+                list = l;
+                prev = list;
             }
-            list = l1;
+            else {
+                prev.next = l;
+                prev = l;
+            }
         }
         return list;
     }
