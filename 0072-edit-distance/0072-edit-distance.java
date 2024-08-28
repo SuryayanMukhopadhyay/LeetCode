507de74e30f21778df7e3b3e18ebce2f1f75dfc7
SuryayanMukhopadhyay <169067813+SuryayanMukhopadhyay@users.noreply.github.com>
2024-08-28 08:54:10 +0530

    Time: 3 ms (96.18%), Space: 44.9 MB (49.36%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,21 +1,30 @@
 class Solution {
-    public int helper(String word1, String word2, int m, int n) {
+    public int helper(String word1, String word2, int m, int n, int[][] memo) {
         if(m == 0) {
             return n;
         }
         if(n == 0) {
             return m;
         }
-        if(word1.charAt(m - 1) == word2.charAt(n - 1)) {
-            return helper(word1, word2, m - 1, n - 1);
+        if(memo[m][n] != -1) {
+            return memo[m][n];
         }
-        int replace = 1 + helper(word1, word2, m - 1, n - 1);
-        int delete = 1 + helper(word1, word2, m - 1, n);
-        int insert = 1 + helper(word1, word2, m, n - 1);
-        return Math.min(replace, Math.min(delete, insert));
+        if(word1.charAt(m - 1) == word2.charAt(n - 1)) {
+            return memo[m][n] = helper(word1, word2, m - 1, n - 1, memo);
+        }
+        int replace = 1 + helper(word1, word2, m - 1, n - 1, memo);
+        int delete = 1 + helper(word1, word2, m - 1, n, memo);
+        int insert = 1 + helper(word1, word2, m, n - 1, memo);
+        return memo[m][n] = Math.min(replace, Math.min(delete, insert));
     }
     public int minDistance(String word1, String word2) {
         int m = word1.length(), n = word2.length();
-        return helper(word1, word2, m, n);
+        int[][] memo = new int[m + 1][n + 1];
+        for(int i = 0; i < m + 1; i++) {
+            for(int j = 0; j < n + 1; j++) {
+                memo[i][j] = -1;
+            }
+        }
+        return helper(word1, word2, m, n, memo);
     }
 }
